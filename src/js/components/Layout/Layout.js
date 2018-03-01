5b3fa8317435e20376b7c14758e4fb366243e3b4
Brian Thomas <bew-dev@users.noreply.github.com>
2018-02-28 17:48:12 -0800

    RELOCATED LAYOUTS - APPLIED BOOTSWATCH - moved layouts into subfolder - applied bootswatch flatly css

--- src/js/components/Layout/Layout.js
+++ src/js/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
 import Footer from "./Footer/Footer";
