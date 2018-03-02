a77209027fb3eeae7d0bf4f00e794ec0fdad44bd
Brian Thomas <bew-dev@users.noreply.github.com>
2018-03-01 21:40:09 -0800

    IMPLEMENTING ROUTING - using react-router and BrowserRouter to implement routes. - This is the intial implementation with no routes yet, but   a working loading page.  routes to follow

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { Router, Route, BrowserRouter, IndexRoute } from "react-router-dom";
+
 // import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
 import Layout from "./components/Layout/Layout";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/" component={Layout}></Route>
+  </BrowserRouter>,
+app);
